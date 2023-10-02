8dfade0e0a4504f3e465b135914bfda32a37c006
recognizeyourself <recognizeyourself909@gmail.com>
2023-10-02 09:20:50 +0530

    14: Storing data in Arrays

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -11,6 +11,7 @@ type FormValues = {
     twitter: string;
     facebook: string;
   };
+  phoneNumbers: string[];
 };
 
 export const YoutubeForm = () => {
@@ -23,6 +24,7 @@ export const YoutubeForm = () => {
         twitter: "",
         facebook: "",
       },
+      phoneNumbers: ["", ""],
     },
   });
 
@@ -120,6 +122,22 @@ export const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="facebook">Facebook</label>
           <input type="text" id="facebook" {...register("social.facebook")} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="primary-phone">Primary Phone Number</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0")}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="secondary-phone">Secondary Phone Number</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1")}
+          />
         </div>
         <button>Submit</button>
       </form>
